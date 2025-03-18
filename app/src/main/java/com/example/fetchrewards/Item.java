77057fd2cad5fcfb438ccb402526833ec3cf2b73
ayushhdesai <aayushdesai88@gmail.com>
2025-03-18 16:27:32 -0400

    File: Edited the Item class to add constructor and getter functions

--- app/src/main/java/com/example/fetchrewards/Item.java
+++ app/src/main/java/com/example/fetchrewards/Item.java
@@ -6,5 +6,23 @@ public class Item {
     private String listId;
     private String name;
 
+    // Constructor for the Item class
+    public Item(int id, String listId, String name) {
+        this.id = id;
+        this.listId = listId;
+        this.name = name;
+    }
 
+    // Getter functions
+    public int getId() {
+        return id;
+    }
+
+    public String getListId() {
+        return listId;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
